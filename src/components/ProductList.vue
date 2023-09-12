fb1d8db10711895207d8e714cc61966a1c0b62ac
Darren Sunandar <darrenmar14@gmail.com>
2023-09-12 16:22:01 +1000

    commit

---
+++ src/components/ProductList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <h2>Product List</h2>
+      <div class="product-card" v-for="product in products" :key="product.id">
+        <img :src="product.image" :alt="product.name" />
+        <h3>{{ product.name }}</h3>
+        <p>{{ product.price }} AUD</p>
+        <button @click="addToCart(product)">Add to Cart</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      products: Array,
+    },
+    methods: {
+      addToCart(product) {
+        this.$emit('add-to-cart', product);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your component styles here */
+  </style>
+  
---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h2>Your Cart</h2>
+      <ul>
+        <li v-for="item in cartItems" :key="item.id">
+          {{ item.name }} - {{ item.price }} AUD
+          <button @click="removeItem(item)">Delete</button>
+        </li>
+      </ul>
+      <p>Total Price: {{ totalPrice }} AUD</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ShoppingCart',
+    props: {
+      cartItems: Array,
+    },
+    methods: {
+      removeItem(item) {
+        this.$emit('remove-item', item);
+      },
+    },
+    computed: {
+        totalPrice() {
+            return this.cartItems.reduce((total, item) => total + item.price, 0);
+        },
+    },
+  };
+  </script>
+  
